752a2c1e5005826ef718bf23443f8c427fbd3e36
SerjPas <passergiy@gmail.com>
2020-02-08 23:02:20 +0200

    I get item objects and add them to the array
    now I can get title (type of car) link on  this car, price in usd and uah and name of city

--- parser_avito.py
+++ parser_avito.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 URL = 'https://auto.ria.com/newauto/marka-jeep/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
            'accept': '*/*'}
+HOST = 'https://auto.ria.com'
 
 
 def get_html(url, params=None):
@@ -16,18 +17,34 @@ def get_content(html):
     Create new object soup from libery BeautifulSoup
     :param html and type of document we work with 'html.parser'
 
+
     """
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('a', class_='na-card-item')
 
-    print(items)
+    cars = []
+    for item in items:
+        uah_price = item.find('span', class_='size15')
+        if uah_price:
+            # замінюємо маркер ' * ' пустою строкою ''
+            uah_price = uah_price.get_text().replace(' • ', '')
+        else:
+            uah_price = 'Цену уточняйте'
+        cars.append({
+            'title': item.find('div', class_='na-card-name').get_text(strip=True),
+            'link': HOST + item.find('span', class_='link').get('href'),
+            'usd_price': item.find('strong', class_='green').get_text(),
+            'uah_price': uah_price,
+            'city': item.find('svg', class_='svg_i16_pin').find_next('span').get_text(),
+        })
+    return cars
 
 
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
         # call the object
-        get_content(html.text)
+        cars = get_content(html.text)
     else:
         print("Error")
 
