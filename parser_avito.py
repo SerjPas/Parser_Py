5b34152287a16e06931b58e18bfb1dffcffe71ed
SerjPas <passergiy@gmail.com>
2020-02-08 22:02:46 +0200

    added main functions

---
+++ parser_avito.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://auto.ria.com/newauto/marka-jeep/'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
+           'accept': '*/*'}
+
+
+def get_html(url, params=None):
+    pass
+
+
+def parse():
+    pass
+
+
+parse()
